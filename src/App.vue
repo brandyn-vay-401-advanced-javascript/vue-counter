69046c26e18632ce345602dff31d1bcae3a194e2
Brandyn Vay <brandynvay@protonmail.com>
2019-10-22 19:33:16 -0700

    fixed

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <h1>Counter</h1>
+    <Counter />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Counter from './components/counter.vue'
 
 export default {
-  name: 'app',
   components: {
-    HelloWorld
+    Counter
   }
 }
 </script>
 
 <style>
-#app {
+div {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
---
+++ src/components/counter.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <h1>{{ count }}</h1>
+    <button @click="decrement()" varient="danger">Decrement</button>
+    <button @click="reset()" varient="reset">Reset</button>
+    <button @click="increment()" varient="success">Increment</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return { count: 0 }
+  },
+  methods: {
+    increment() {
+      this.count += 1
+    },
+    decrement() {
+      this.count -= 1
+    },
+    reset() {
+      this.count = 0
+    }
+  },
+  mounted() {
+    console.log('App mounted!')
+    if (localStorage.getItem('count'))
+      this.count = JSON.parse(localStorage.getItem('count'))
+  },
+  watch: {
+    count: {
+      handler() {
+        console.log('Count changed!')
+        localStorage.setItem('count', JSON.stringify(this.count))
+      },
+      deep: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+span {
+  font-weight: bold;
+  font-size: 40px / 2;
+}
+div {
+  text-align: center;
+  padding: 1em;
+  background-color: #fafafa;
+  border: 1px solid #ccc;
+  border-radius: 3px;
+}
+</style>
